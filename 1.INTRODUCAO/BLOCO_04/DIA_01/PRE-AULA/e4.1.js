4f62bfd48e893bf871b9e8af8f6cf22b80edaef0
thosijulio <thosijulio@gmail.com>
2020-12-29 23:20:49 -0300

    resolucao exercicio 4.1

---
+++ 1.INTRODUCAO/BLOCO_04/DIA_01/PRE-AULA/e4.1.js
@@ -0,0 +1,18 @@
+let statusCandidato;
+
+switch (statusCandidato) {
+    case 'aprovado':
+        console.log ('Parabéns, você foi aprovado!');
+        break;
+
+    case 'lista':
+        console.log ('Você está na nossa lista de espera');
+        break;
+
+    case 'reprovado':
+        console.log ('Você foi reprovado');
+        break;
+
+    default:
+        console.log ('Não se aplica');
+}
