e2857d9e960010022fc16bc5dac09f9aefe74b91
thosijulio <thosijulio@gmail.com>
2020-12-30 00:39:11 -0300

    resolução exercício 9

---
+++ 1.INTRODUCAO/BLOCO_04/DIA_01/e09.js
@@ -0,0 +1,8 @@
+let number1 = 4, number2 = 3, number3 = 2;
+
+if ((number1 % 2) !== 0 || (number2 % 2) !== 0 || (number3 % 2) !== 0){
+    console.log ('true');
+}
+else {
+    console.log ('false');
+}
