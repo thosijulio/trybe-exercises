41848eff6d95b3373e5fbee076b44578680a60c1
thosijulio <thosijulio@gmail.com>
2020-12-30 00:36:42 -0300

    resolução exercício 8

---
+++ 1.INTRODUCAO/BLOCO_04/DIA_01/e08.js
@@ -0,0 +1,8 @@
+let number1 = 5, number2 = 7, number3 = 6;
+
+if ((number1 % 2) == 0 || (number2 % 2) == 0 || (number3 % 2) == 0){
+    console.log ('true');
+}
+else {
+    console.log ('false');
+}
