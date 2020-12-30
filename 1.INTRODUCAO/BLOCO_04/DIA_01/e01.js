12799594e3f5805ca8de879ad2efef50dfa7e537
thosijulio <thosijulio@gmail.com>
2020-12-29 23:30:32 -0300

    resolucao exercicio 1

---
+++ 1.INTRODUCAO/BLOCO_04/DIA_01/e01.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 5;
+let resultado;
+
+resultado = a + b;
+console.log(resultado);
+
+resultado = a - b;
+console.log(resultado);
+
+resultado = a * b;
+console.log(resultado);
+
+resultado = a / b;
+console.log(resultado);
+
+resultado = a % b;
+console.log(resultado);
