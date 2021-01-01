8f7ed1105c6133a9f6be825dd476e880492d0a51
thosijulio <thosijulio@gmail.com>
2021-01-01 16:27:31 -0300

    Resolução exercício 5

---
+++ 1.INTRODUCAO/BLOCO_04/DIA_04/PARTE-1/e05.js
@@ -0,0 +1,25 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: 'Sim',
+};
+
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: 'Sim',
+};
+
+for(let key in info) {
+    if (key !== 'recorrente') {
+        console.log (info[key] + ' e ' + info2[key]);
+    }
+    else if (info.recorrente === info2.recorrente) {
+        console.log ('Ambom recorrentes');
+    }
+    else {
+        console.log (info[key] + ' e ' + info2[key]);
+    }
+}
