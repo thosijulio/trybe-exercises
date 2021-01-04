70f223e19d6eaa9e7004b40b4cd0ad2a25e35340
thosijulio <thosijulio@gmail.com>
2021-01-04 00:39:21 -0300

    Resolução exercício 1

---
+++ 1.INTRODUCAO/BLOCO_05/DIA_02/exercicios.js
@@ -0,0 +1,8 @@
+let exercicio1 = document.createElement('h1');
+exercicio1.innerText = 'Exercício 5.2 - JavaScript DOM';
+exercicio1.style.textAlign = 'center';
+exercicio1.style.backgroundColor = 'Green';
+exercicio1.style.margin = '0';
+document.body.style.margin = '0';
+
+document.body.appendChild(exercicio1);
