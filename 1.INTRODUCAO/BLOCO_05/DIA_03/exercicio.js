a4db3fb0ac1108fd65cb343fd79cf2fb0be5312a
thosijulio <thosijulio@gmail.com>
2021-01-04 20:50:58 -0300

    Resolução exercício 2

--- 1.INTRODUCAO/BLOCO_05/DIA_03/exercicio.js
+++ 1.INTRODUCAO/BLOCO_05/DIA_03/exercicio.js
@@ -13,7 +13,7 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-function numberDays () {
+function exercicio1 () {
     const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
     for (let key in dezDaysList) {
         let dayArray = document.createElement('li');
@@ -31,4 +31,12 @@ function numberDays () {
     }
 }
 
-numberDays();
+exercicio1();
+
+function exercicio2 (feriados) {
+    const botaoExerc2 = document.createElement ('button');
+    const containerBotoes = document.querySelector('.buttons-container');
+    botaoExerc2.id = 'btn-holiday';
+    containerBotoes.appendChild(botaoExerc2);
+}
+exercicio2("feriados");
