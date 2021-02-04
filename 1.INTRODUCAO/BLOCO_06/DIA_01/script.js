9eb57b46b32106d2a9b7bb4c5693fc6630759248
thosijulio <thosijulio@gmail.com>
2021-02-03 21:12:34 -0300

    trabalhando com a data

--- 1.INTRODUCAO/BLOCO_06/DIA_01/script.js
+++ 1.INTRODUCAO/BLOCO_06/DIA_01/script.js
@@ -1,4 +1,39 @@
-let test = document.querySelector('#positionDescription');
-let oi = document.createElement('p');
-oi.innerHTML = 'ooi';
-test.appendChild(oi)
+function verificarData () {
+  let dataInput = document.querySelector('#initialDay');
+  let submitButton = document.querySelector('#submit');
+  dataInput.addEventListener('change', function () {
+    if (dataInput.value.length === 8) {
+      if ((dataInput.value[0] + dataInput.value[1]) > 0 && (dataInput.value[0] + dataInput.value[1]) <= 31) {
+        if ((dataInput.value[2] + dataInput.value[3]) > 0 && (dataInput.value[2] + dataInput.value[3]) <= 12) {
+          if ((dataInput.value[4] + dataInput.value[5] + dataInput.value[6] + dataInput.value[7]) > 0) {
+              let dataFormated = dataInput.value.substr(0,2) + '/' + dataInput.value.substr(2,2) + '/' + dataInput.value.substr(4,4);
+              dataInput.value = dataFormated;
+          }
+          else {
+            alert('Ano inválido');
+            dataInput.value = '';
+          }
+        }
+        else {
+          alert('Mês inválido');
+          dataInput.value = '';
+        }
+      }
+      else {
+        alert('Dia inválido');
+        dataInput.value = '';
+      }
+    }
+    else {
+        alert('Data inválida');
+        dataInput.value = '';
+    }
+  })
+}
+
+function botaoExerc3 () {
+
+}
+
+botaoExerc3 ();
+verificarData ();
