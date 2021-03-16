46f7ded268ee8e7f662907a678e45e2ac27b21e0
thosijulio <thosijulio@gmail.com>
2021-03-16 16:10:04 -0300

    continuacao do exercicio 1

---
+++ 1.INTRODUCAO/BLOCO_06/DIA_01/script.js
@@ -0,0 +1,37 @@
+function createStates() {
+  let estados = {
+    'AC': 'Acre',
+    'AL': 'Alagoas',
+    'AP': 'Amapá',
+    'AM': 'Amazonas',
+    'BA': 'Bahia',
+    'CE': 'Ceará',
+    'DF': 'Distrito Federal',
+    'ES': 'Espírito Santo',
+    'GO': 'Goiás',
+    'MA': 'Maranhão',
+    'MT': 'Mato Grosso',
+    'MS': 'Mato Grosso do Sul',
+    'MG': 'Minas Gerais',
+    'PA': 'Pará',
+    'PB': 'Paraíba',
+    'PR': 'Paraná',
+    'PE': 'Pernambuco',
+    'PI': 'Piauí',
+    'RJ': 'Rio de Janeiro',
+    'RN': 'Rio Grande do Norte',
+    'RS': 'Rio Grande do Sul',
+    'RO': 'Rondônia',
+    'RR': 'Roraima',
+    'SC': 'Santa Catarina',
+    'SP': 'São Paulo',
+    'SE': 'Sergipe',
+    'TO': 'Tocantins'
+}
+let tagSelect = document.getElementById('state');
+let estadosLenght = Object.keys(estados).length;
+for(let key in estados) {
+  console.log(key);
+}
+}
+createStates();
