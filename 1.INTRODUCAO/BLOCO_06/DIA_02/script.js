67f38a3ac3dd32afd87bc286639d052bdca3844a
thosijulio <thosijulio@gmail.com>
2021-03-18 23:04:27 -0300

    adicionando bibliotecas

--- 1.INTRODUCAO/BLOCO_06/DIA_02/script.js
+++ 1.INTRODUCAO/BLOCO_06/DIA_02/script.js
@@ -1,108 +1,135 @@
-function verificarData () {
-  let dataInput = document.querySelector('#initialDay');
-  let submitButton = document.querySelector('#submit');
-  dataInput.addEventListener('change', function () {
-    if (dataInput.value.length === 8) {
-      if ((dataInput.value[0] + dataInput.value[1]) > 0 && (dataInput.value[0] + dataInput.value[1]) <= 31) {
-        if ((dataInput.value[2] + dataInput.value[3]) > 0 && (dataInput.value[2] + dataInput.value[3]) <= 12) {
-          if ((dataInput.value[4] + dataInput.value[5] + dataInput.value[6] + dataInput.value[7]) > 0) {
-              let dataFormated = dataInput.value.substr(0,2) + '/' + dataInput.value.substr(2,2) + '/' + dataInput.value.substr(4,4);
-              dataInput.value = dataFormated;
-          }
-          else {
-            alert('Ano inválido');
-            dataInput.value = '';
-          }
-        }
-        else {
-          alert('Mês inválido');
-          dataInput.value = '';
+function createStates() {
+  let estados = {
+    'AC': 'Acre',
+    'AL': 'Alagoas',
+    'AP': 'Amapá',
+    'AM': 'Amazonas',
+    'BA': 'Bahia',
+    'CE': 'Ceará',
+    'DF': 'Distrito Federal',
+    'ES': 'Espírito Santo',
+    'GO': 'Goiás',
+    'MA': 'Maranhão',
+    'MT': 'Mato Grosso',
+    'MS': 'Mato Grosso do Sul',
+    'MG': 'Minas Gerais',
+    'PA': 'Pará',
+    'PB': 'Paraíba',
+    'PR': 'Paraná',
+    'PE': 'Pernambuco',
+    'PI': 'Piauí',
+    'RJ': 'Rio de Janeiro',
+    'RN': 'Rio Grande do Norte',
+    'RS': 'Rio Grande do Sul',
+    'RO': 'Rondônia',
+    'RR': 'Roraima',
+    'SC': 'Santa Catarina',
+    'SP': 'São Paulo',
+    'SE': 'Sergipe',
+    'TO': 'Tocantins'
+}
+
+let tagSelect = document.getElementById('state');
+let estadosLenght = Object.keys(estados).length;
+for(let key in estados) {
+  let selectEstados = document.createElement('option');
+  selectEstados.value = key;
+  selectEstados.innerText = key + ' - ' + estados[key];
+  tagSelect.appendChild(selectEstados);
+}
+}
+
+function ajustarData() {
+  let campoData = document.getElementById('dataInicio');
+  campoData.addEventListener('input', function(event) {
+    if(campoData.value.length == 2) {
+      if(campoData.value[1] !== '/') {
+        campoData.value += '/';
+      }
+    }
+    if(campoData.value.length == 5) {
+      if(campoData.value[4] !== '/') {
+        campoData.value += '/';
+      }
+    }
+  })
+}
+
+function verifyDate() {
+    let dataInserida = document.getElementById('dataInicio');
+    dataInserida.addEventListener('change', function() {
+      let verificaDia = parseInt((dataInserida.value[0] + dataInserida.value[1]));
+      let verificaMes = parseInt((dataInserida.value[3] + dataInserida.value[4]));
+      let verificaAno = parseInt((dataInserida.value[6] + dataInserida.value[7] + dataInserida.value[8] + dataInserida.value[9]));
+
+      if (verificaDia) {
+        if(verificaDia > 31 || verificaDia <= 0) {
+          alert("Dia inválido!");
+          dataInserida.value = '';
         }
       }
       else {
-        alert('Dia inválido');
-        dataInput.value = '';
+        alert("Dia inválido!");
+        dataInserida.value = '';
       }
-    }
-    else {
-        alert('Data inválida');
-        dataInput.value = '';
-    }
+      if (verificaMes) {
+        if(verificaMes <= 0 || (verificaMes > 12)) {
+          alert("Mês inválido!");
+          dataInserida.value = '';
+        }
+      }
+      else {
+        alert('Mês inválido!');
+        dataInserida.value = '';
+      }
+      if (verificaAno) {
+        if(verificaAno.value <= 0) {
+          alert('Ano inválido');
+          dataInserida.value = '';
+        }
+      }
+      else {
+        alert('Ano inválido');
+        dataInserida.value = '';
+      }
+    })
+}
+
+function showContent() {
+  let buttonSubmit = document.getElementById('botao-enviar');
+  buttonSubmit.addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('1').innerText = 'Nome: ' + document.getElementById('name').value;
+    document.getElementById('2').innerText = 'Email: ' + document.getElementById('email').value;
+    document.getElementById('3').innerText = 'CPF: ' + document.getElementById('CPF').value;
+    document.getElementById('4').innerText = 'Endereço: ' + document.getElementById('address').value;
+    document.getElementById('5').innerText = 'Cidade: ' + document.getElementById('city').value;
+    document.getElementById('6').innerText = 'Estado: ' + document.getElementById('state').value;
+    document.getElementById('7').innerText = 'Tipo: ' + document.querySelector('input[name="residence-type"]:checked').value;
+    document.getElementById('8').innerText = 'Resumo do curriculo: ' + document.getElementById('resumoCV').value;
+    document.getElementById('9').innerText = 'Cargo: ' + document.getElementById('cargo').value;
+    document.getElementById('10').innerText = 'Descrição do cargo: ' + document.getElementById('descricaoCargo').value;
+    document.getElementById('11').innerText = 'Data de início: ' + document.getElementById('dataInicio').value;
   })
 }
 
-function limparCampos () {
-  let inputs = document.querySelectorAll('input');
-  let textArea = document.querySelectorAll('textarea');
-  let selects = document.querySelectorAll('select');
-
-  let botaoLimpa = document.querySelector('#clearForm');
-  botaoLimpa.addEventListener('click', function () {
+function eraseAll() {
+  let buttonErase = document.getElementById('eraseButton');
+  buttonErase.addEventListener('click', function() {
+    let div = document.getElementById('resumoDados').childNodes;
+    let inputs = document.getElementsByTagName('input');
+    for(index = 0; index < div.length; index += 1){
+      div[index].innerText = '';
+    }
     for(index = 0; index < inputs.length; index += 1) {
       inputs[index].value = '';
     }
-    for(index = 0; index < textArea.length; index += 1) {
-        textArea[index].value = '';
-    }
-    for (index = 0; index < selects.length; index += 1) {
-        selects[index].value = '';
-    }
-    let div = document.querySelector('.dadosPreenchidos');
-    if (div !== null) {
-      document.body.removeChild(div);
-    }
+    document.getElementsByTagName('textarea')[0].value = '';
   })
 }
 
-function criarEstados () {
-    let selectEstados = document.querySelector('#state');
-    let estados = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MS','MT','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO',];
-    for(index = 0; index < estados.length; index += 1) {
-        let optionEstados = document.createElement ('option');
-        optionEstados.id = estados[index];
-        optionEstados.value = estados[index];
-        optionEstados.name = 'state';
-        optionEstados.innerText = estados[index];
-        selectEstados.appendChild(optionEstados);
-        selectEstados.value = '';
-    }
-}
-
-function showForm () {
-  let buttonForm = document.querySelector('#showForm');
-    buttonForm.addEventListener('click', function (event) {
-      event.preventDefault();
-      let testeIf = document.querySelector('.dadosPreenchidos');
-      if (testeIf === null) {
-        let buttonForm = document.querySelector('#showForm');
-        let inputs = document.querySelectorAll('input');
-        let selects = document.querySelector('select');
-        let labels = document.querySelectorAll('label');
-        let criarDiv = document.createElement('div');
-        criarDiv.className = 'dadosPreenchidos';
-        document.body.appendChild(criarDiv);
-        let divFeita = document.querySelector('.dadosPreenchidos');
-    
-      // let inputs = document.querySelector('input[name="typeHome"]:checked');
-        for (index = 0; index <= 4; index += 1) {
-          let nomeCampo = document.createElement('p');
-          nomeCampo.innerText = inputs[index].name + ': ' + inputs[index].value;
-          divFeita.appendChild(nomeCampo);
-        }
-        let nomeCampo = document.createElement('p');
-        nomeCampo.innerText = selects.name + ': ' + selects.value;
-        divFeita.appendChild(nomeCampo);
-
-        let nomeCampo2 = document.createElement('p');
-        let tipoResidencia = document.querySelector('input[name="Tipo"]:checked');
-        nomeCampo2.innerText = 'Tipo de Residencia: ' + tipoResidencia.value;
-        divFeita.appendChild(nomeCampo2);
-
-      }
-    })
-  }
-
-
-criarEstados ();
-limparCampos ();
-showForm ();
+verifyDate();
+createStates();
+ajustarData();
+showContent();
+eraseAll();
