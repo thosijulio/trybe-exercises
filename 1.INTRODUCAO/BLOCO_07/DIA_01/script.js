a64e4ba484671ed29964f960d2a610213602acbe
thosijulio <thosijulio@gmail.com>
2021-03-23 19:58:21 -0300

    resolucao exercicio 1 e 2

---
+++ 1.INTRODUCAO/BLOCO_07/DIA_01/script.js
@@ -0,0 +1,16 @@
+/* Parte 1 */
+
+//exercicio 1
+const testingScope = (escopo) => {
+  let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+  escopo ? console.log(`${ifScope} ótimo, fui utilizada no escopo`) : console.log(`não devo ser utilizada for do meu escopo (else)`);
+}
+
+testingScope(true);
+
+//exercicio 2
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const arrayCrescente = (array) => console.log(`Os números ${array.sort((a, b) => a - b)} se encontram ordenados de forma crescente!`);
+
+arrayCrescente(oddsAndEvens);
