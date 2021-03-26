f977cf8d744e427bd1c383b034cfd5da9d47e898
thosijulio <thosijulio@gmail.com>
2021-03-26 10:04:14 -0300

    resolucao exercicio 2 - Parte 1

--- 1.INTRODUCAO/BLOCO_07/DIA_02/script.js
+++ 1.INTRODUCAO/BLOCO_07/DIA_02/script.js
@@ -1,11 +1,3 @@
-/* Agora você vai fazer alguns exercícios de fixação.
-Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
-Note que o parâmetro da função já está sendo passado na chamada da função.
-Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de muzzarella, calabresa e Coca-Cola Zero é R$ 50,00."
-Modifique o nome da pessoa compradora.
-Modifique o valor total da compra para R$ 50,00. */
-
-
 const order = {
   name: 'Rafael Andrade',
   phoneNumber: '11-98763-1416',
@@ -47,8 +39,11 @@ const customerInfo = (order) => console.log(`Olá ${order.order.delivery.deliver
 customerInfo(order);
 
 const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
-
+  const newBuyer = order.name = 'Luis Silva';
+  const pizzas = Object.keys(order.order.pizza);
+  const drinks = order.order.drinks.coke.type;
+  const newTotal = order.payment = 50;
+  console.log(`Olá ${newBuyer}, o total do seu pedido de ${pizzas} e ${drinks} é R$ ${newTotal},00.`);
 }
 
 orderModifier(order);
