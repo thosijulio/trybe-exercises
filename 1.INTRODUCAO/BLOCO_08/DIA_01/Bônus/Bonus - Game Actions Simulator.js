444a01d3867560e147b8b3394794c02728b53a58
thosijulio <thosijulio@gmail.com>
2021-03-30 12:04:38 -0300

    resolucao exercicio 3 - Bonus

--- 1.INTRODUCAO/BLOCO_08/DIA_01/Bônus/Bonus - Game Actions Simulator.js
+++ 1.INTRODUCAO/BLOCO_08/DIA_01/Bônus/Bonus - Game Actions Simulator.js
@@ -1,5 +1,6 @@
-/* Crie uma função que reotorna o dano causado pelo warrior .
-O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo). */
+/* Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0. */
 
 const mage = {
   healthPoints: 130,
@@ -25,3 +26,16 @@ const battleMembers = { mage, warrior, dragon };
 
 const dragonDamage = (strength) => Math.floor(Math.random() * (strength - 15 + 1) + 15);
 const warriorDamage = (strength, weaponDmg) => Math.floor(Math.random() * ((strength * weaponDmg) - strength + 1) + strength);
+const mageTurn = (intelligence, mana) => {
+  const turnResult = {};
+  if(mana < 15) {
+    turnResult.dano = 0;
+    turnResult.mana = mana;
+    return turnResult;
+  }
+  const turnResult = {
+    dano: Math.floor(Math.random() * ((intelligence * 2) - intelligence + 1) + intelligence),
+    mana: 15,
+  }
+  return turnResult;
+}
