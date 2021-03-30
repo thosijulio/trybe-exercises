c392df4f7745bdd3892bf62cb14402581669d1f0
thosijulio <thosijulio@gmail.com>
2021-03-30 12:42:00 -0300

    resolucao exercicio bonus

--- 1.INTRODUCAO/BLOCO_08/DIA_01/Bônus/Bonus - Game Actions Simulator.js
+++ 1.INTRODUCAO/BLOCO_08/DIA_01/Bônus/Bonus - Game Actions Simulator.js
@@ -1,7 +1,3 @@
-/* Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
-O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
-A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0. */
-
 const mage = {
   healthPoints: 130,
   intelligence: 45,
@@ -24,18 +20,61 @@ const dragon = {
 
 const battleMembers = { mage, warrior, dragon };
 
-const dragonDamage = (strength) => Math.floor(Math.random() * (strength - 15 + 1) + 15);
-const warriorDamage = (strength, weaponDmg) => Math.floor(Math.random() * ((strength * weaponDmg) - strength + 1) + strength);
-const mageTurn = (intelligence, mana) => {
-  const turnResult = {};
-  if(mana < 15) {
-    turnResult.dano = 0;
-    turnResult.mana = mana;
-    return turnResult;
+const dragonAttack = (dragon) => {
+  const minDmg = 15;
+  const { strength } = dragon;
+  const dragonDmg = Math.floor((Math.random() * (strength - minDmg) + minDmg));
+  return dragonDmg;
+};
+const warriorAttack = (warrior) => {
+  const weaponDmg = warrior.weaponDmg;
+  const minDmg = warrior.strength;
+  const maxDmg = minDmg * weaponDmg;
+  const warriorDamage = Math.floor((Math.random() * (maxDmg - minDmg)) + minDmg);
+  return warriorDamage;
+};
+const mageAttack = (mage) => {
+  const mageMana = mage.mana;
+  const minDmg = mage.intelligence;
+  const maxDmg = minDmg * 2;
+  const turnStats = {
+    manaSpent: 0,
+    damageDealt: 'Not enough mana...',
+  };
+
+  if (mageMana > 15) {
+    const mageDamage = Math.floor((Math.random() * (maxDmg - minDmg)) + minDmg);
+    turnStats.manaSpent = 15;
+    turnStats.damageDealt = mageDamage;
+    return turnStats;
   }
-  const turnResult = {
-    dano: Math.floor(Math.random() * ((intelligence * 2) - intelligence + 1) + intelligence),
-    mana: 15,
-  }
-  return turnResult;
-}
+    return turnStats;
+};
+
+const gameActions = {
+  warriorTurn: (warriorAttack) => {
+    const warriorDamage = warriorAttack(warrior);
+    warrior.damage = warriorDamage;
+    dragon.healthPoints -= warriorDamage;
+  },
+  mageTurn: (mageAttack) => {
+    const mageTurnStats = mageAttack(mage);
+    const mageDamage = mageTurnStats.damageDealt;
+    const { manaSpent } = mageTurnStats;
+    mage.damage = mageDamage;
+    mage.mana -= manaSpent;
+    dragon.healthPoints -= mageDamage;
+  },
+  monsterTurn: (monsterAttack) => {
+    const dragonDamage = monsterAttack(dragon);
+    mage.healthPoints -= dragonDamage;
+    warrior.healthPoints -= dragonDamage;
+    dragon.damage = dragonDamage;
+  },
+  turnResults: () => battleMembers,
+};
+
+gameActions.warriorTurn(warriorAttack);
+gameActions.mageTurn(mageAttack);
+gameActions.monsterTurn(dragonAttack);
+console.log(gameActions.turnResults());
