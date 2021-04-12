02a8a671a8d26a5b6de9289335ca712582ad293f
thosijulio <thosijulio@gmail.com>
2021-04-12 18:36:31 -0300

    resolucao exercicio 2 usando reduce

--- 1.INTRODUCAO/BLOCO_08/DIA_05/exercicio2.js
+++ 1.INTRODUCAO/BLOCO_08/DIA_05/exercicio2.js
@@ -1,16 +1,9 @@
 const assert = require('assert');
 
-const sum = (...numbers) => {
-    let sums = 0;
-    numbers.forEach((number) => sums+= number);
-    return sums;
-}
-
+const sum = ((...numbers) => numbers.reduce((acc, curr) => acc + curr, 0));
 
 assert.strictEqual(sum(), 0);
 assert.strictEqual(sum(1), 1);
 assert.strictEqual(sum(1, 2), 3);
 assert.strictEqual(sum(1, 2, 3), 6);
 assert.strictEqual(sum(1, 2, 3, 4), 10);
-
- 
