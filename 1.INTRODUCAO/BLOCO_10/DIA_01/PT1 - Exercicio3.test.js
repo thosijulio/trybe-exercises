ce517d39070a163bb59c555288a7983f3f149022
thosijulio <thosijulio@gmail.com>
2021-04-27 20:18:37 -0300

    resolucao exercicio 3.3 - Parte 1

--- 1.INTRODUCAO/BLOCO_10/DIA_01/PT1 - Exercicio3.test.js
+++ 1.INTRODUCAO/BLOCO_10/DIA_01/PT1 - Exercicio3.test.js
@@ -7,4 +7,9 @@ describe('Testes do Exercício 3', () => {
   test('O retorno de "myRemoveWithoutCopy([1, 2, 3, 4], 3)" não retorna [1, 2, 3, 4]', () => {
     expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
   });
+  test('O array passado por parâmentro para a função "myRemoveWithoutCopy" não sofreu alterações', () => {
+    const arr = [5, 6, 7, 8];
+    myRemoveWithoutCopy(arr, 5);
+    expect(arr).not.toEqual([5, 6, 7, 8]);
+  });
 });
