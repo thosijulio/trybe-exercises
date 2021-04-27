2bb458b40bb633853c784865895d9f0a6d3bc4ce
thosijulio <thosijulio@gmail.com>
2021-04-27 19:57:41 -0300

    resolucao exercicio 2.3 - Parte 1

--- 1.INTRODUCAO/BLOCO_10/DIA_01/PT1 - Exercicio2.test.js
+++ 1.INTRODUCAO/BLOCO_10/DIA_01/PT1 - Exercicio2.test.js
@@ -7,4 +7,9 @@ describe('Testes do Exercício 2', () => {
   test('2. O retorno de "myRemove([1, 2, 3, 4], 3)" não retorna "[1, 2, 3, 4]"', () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
   });
+  test('3. O array passado por parâmetro não sofreu alterações', () => {
+    const arr = [5, 6, 7, 8];
+    myRemove(arr, 6);
+    expect(arr).toEqual([5, 6, 7, 8]);
+  });
 });
