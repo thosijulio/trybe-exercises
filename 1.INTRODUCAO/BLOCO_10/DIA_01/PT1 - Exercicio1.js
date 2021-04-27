11204284830708135935876e799c9a0e83f41b9e
thosijulio <thosijulio@gmail.com>
2021-04-27 18:36:24 -0300

    resolucao exercicio 1.1

--- 1.INTRODUCAO/BLOCO_10/DIA_01/PT1 - Exercicio1.js
+++ 1.INTRODUCAO/BLOCO_10/DIA_01/PT1 - Exercicio1.js
@@ -1,5 +1,3 @@
-const assert = require('assert');
-
 function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('parameters must be numbers');
@@ -8,12 +6,4 @@ function sum(a, b) {
   return a + b;
 }
 
-assert.strictEqual(typeof(sum), 'function');
-assert.strictEqual(sum(4, 5), 9);
-assert.strictEqual(sum(0, 0), 0);
-assert.throws(() => {
-  sum(4, '5');
-})
-assert.throws(() => {
-  sum(4, '5');
-}, /^Error: parameters must be numbers$/);
+module.exports = sum;
