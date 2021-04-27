3ef31ec8a79f94471f77acd1e2f88165208771f9
thosijulio <thosijulio@gmail.com>
2021-04-27 19:42:36 -0300

    resolucao exercicio 2.1

--- 1.INTRODUCAO/BLOCO_10/DIA_01/PT1 - Exercicio1.test.js
+++ 1.INTRODUCAO/BLOCO_10/DIA_01/PT1 - Exercicio1.test.js
@@ -2,10 +2,10 @@ const sum = require('./PT1 - Exercicio1.js');
 
 describe('Testes do Exercício 1', () => {
   test('1. O retorno de sum(4, 5) é igual a 9', () => {
-    expect(sum(4, 5)).toEqual(9);
+    expect(sum(4, 5)).toBe(9);
   });
   test('2. O retorno de sum(0, 0) é igual a 0', () => {
-    expect(sum(0, 0)).toEqual(0);
+    expect(sum(0, 0)).toBe(0);
   });
   test('3. Sum deve lançar erro quando passado string por parâmetro', () => {
     expect(() => sum(4, '5')).toThrow();
