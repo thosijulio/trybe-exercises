3a11d04df3367fb674929b0b5f4ee38bf2c323c9
thosijulio <thosijulio@gmail.com>
2021-04-28 23:27:42 -0300

    resolucao exercicios da parte 2 - Exercicio 1

--- 1.INTRODUCAO/BLOCO_10/DIA_01/Parte 2/PT2 - Exercicio1.test.js
+++ 1.INTRODUCAO/BLOCO_10/DIA_01/Parte 2/PT2 - Exercicio1.test.js
@@ -15,4 +15,13 @@ describe('Testes do Exerício 1', () => {
     const consoantes = 'b, c, d, f, g, h, j, k, l, m, n, p, q, r, s, t, v, w, x, y, z';
     expect(encode(consoantes)).toEqual(consoantes);
   });
+  test('Demais números não são convertidos usando decode', () => {
+    expect(decode('6, 7, 8, 9, 0')).toEqual('6, 7, 8, 9, 0');
+  });
+  test('O parametro passado para encode tem o mesmo numero de caracteres que o retorno', () => {
+    expect(encode('amoeba').length).toEqual(('amoeba').length);
+  });
+  test('O parametro passado para encode tem o mesmo numero de caracteres que o retorno', () => {
+    expect(encode('g0l31r0').length).toEqual(('g0l31r0').length);
+  });
 });
