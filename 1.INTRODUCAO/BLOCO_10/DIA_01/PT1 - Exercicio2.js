dee2eda751f20599a243a0e9b39ae8acfe663e5d
thosijulio <thosijulio@gmail.com>
2021-04-28 23:19:08 -0300

    organizacao de pastas e resolcucao dos exercicios da Parte 2 - Exercicios 1.1, 1.2, 1.3

--- 1.INTRODUCAO/BLOCO_10/DIA_01/PT1 - Exercicio2.js
+++
@@ -1,9 +0,0 @@
-function myRemove(arr, item) {
-  let newArr = [];
-  for (let index = 0; index < arr.length; index += 1) {
-    item !== arr[index] ? newArr.push(arr[index]) : 0;
-  }
-  return newArr;
-}
-
-module.exports = myRemove;
