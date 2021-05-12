ee4918d6ab1187ae74ff949a0b18a87529630a64
thosijulio <thosijulio@gmail.com>
2021-05-12 09:34:11 -0300

    resolucao exercicio 2

--- 2.FRONT-END/BLOCO_12/DIA_01/exercise-pokedex-state-master/src/App.js
+++ 2.FRONT-END/BLOCO_12/DIA_01/exercise-pokedex-state-master/src/App.js
@@ -3,13 +3,31 @@ import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.changeList = this.changeList.bind(this);
+    this.state = {
+      pokemons,
+    }
+  }
+
+  changeList(type) {
+    this.setState((oldState, _props) => ({
+      pokemons: pokemons.filter((pokemon) => pokemon.type === type),
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Pokedex </h1>
+        <Pokedex pokemons={this.state.pokemons} />
+        <button onClick={ () => (this.changeList('Fire')) } type="button">Fire</button>
+        <button onClick={ () => (this.changeList('Psychic')) } type="button">Psychic</button>
+      </div>
+    );
+  }
 }
 
 export default App;
