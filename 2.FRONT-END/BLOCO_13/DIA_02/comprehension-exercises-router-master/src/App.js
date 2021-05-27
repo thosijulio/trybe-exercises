b5418b506bdae379349006662324f68f845127c6
thosijulio <thosijulio@gmail.com>
2021-05-26 23:37:55 -0300

    resolucao exercicio 3

--- 2.FRONT-END/BLOCO_13/DIA_02/comprehension-exercises-router-master/src/App.js
+++ 2.FRONT-END/BLOCO_13/DIA_02/comprehension-exercises-router-master/src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <Route path="/" component={ Home } />
+        <Route path="/about" component={ About } />
         <Link to="/"> Home </Link>
         <Link to="/about"> About </Link>
         <Link to="/users"> Users </Link>
