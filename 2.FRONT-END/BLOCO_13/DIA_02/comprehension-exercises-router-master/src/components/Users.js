f9a4ef9acc0588b8285287534e9c07109157704b
thosijulio <thosijulio@gmail.com>
2021-05-27 00:05:36 -0300

    resolucao exercicio 9

--- 2.FRONT-END/BLOCO_13/DIA_02/comprehension-exercises-router-master/src/components/Users.js
+++ 2.FRONT-END/BLOCO_13/DIA_02/comprehension-exercises-router-master/src/components/Users.js
@@ -1,12 +1,16 @@
 // arquivo Users.js
 import React from 'react';
 
-const Users = ({ greetingMessage = 'Hi There' }) => (
+const Users = (props) => {
+  const { greetingMessage } = props;
+  const { id } = props.match.params;
+  return (
   <div>
     <h2> Users </h2>
-    <p> {greetingMessage}, this is my awesome Users component </p>
+    <p> {greetingMessage}, this is my awesome Users { id } component </p>
   </div>
-);
+  )
+};
 
 export default Users;
 
