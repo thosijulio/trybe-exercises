7fa0483f246fc3378cb9c590abb83602deebc53f
thosijulio <thosijulio@gmail.com>
2021-05-26 23:35:56 -0300

    resolucao exercicio 2

--- 2.FRONT-END/BLOCO_13/DIA_02/comprehension-exercises-router-master/src/App.js
+++ 2.FRONT-END/BLOCO_13/DIA_02/comprehension-exercises-router-master/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-import BrowserRouter from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import About from './components/About';
+import Users from './components/Users';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Home />
+        <Link to="/"> Home </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/users"> Users </Link>
       </BrowserRouter>
     );
   }
