b2373474f0aa5d1dba00bcb1733f39cc18e35ff3
thosijulio <thosijulio@gmail.com>
2021-05-27 00:26:10 -0300

    resolucao exercicio 13

--- 2.FRONT-END/BLOCO_13/DIA_02/comprehension-exercises-router-master/src/App.js
+++ 2.FRONT-END/BLOCO_13/DIA_02/comprehension-exercises-router-master/src/App.js
@@ -3,19 +3,28 @@ import Home from './components/Home';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import About from './components/About';
 import Users from './components/Users';
+import StrictAccess from './StrictAccess';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/users/:id" render={(props) => <Users { ...props } greetingMessage="Good Morning"/>} />          
+          <Route path="/users/:id" render={ (props) => <Users { ...props } greetingMessage="Good Morning"/> } />          
+          <Route
+            path="/strict-access"
+            render={ (props) => <StrictAccess
+              {...props}
+              user={ ({ username: 'teste', password: '' }) }
+            />}
+          />
           <Route path="/about" component={ About } />
           <Route exact path="/" component={ Home } />
         </Switch>
         <Link to="/"> Home </Link>
         <Link to="/about"> About </Link>
         <Link to="/users"> Users </Link>
+        <Link to="/strict-access">Strict Access</Link>
       </BrowserRouter>
     );
   }
