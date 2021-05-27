a3debfd88e6b38ecd33b671894ea708d22405df2
thosijulio <thosijulio@gmail.com>
2021-05-27 18:23:04 -0300

    resolucao exercicio 2 e 3

--- 2.FRONT-END/BLOCO_13/DIA_02/exercise-pokedex-router-master/src/App.js
+++ 2.FRONT-END/BLOCO_13/DIA_02/exercise-pokedex-router-master/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import PokemonDetails from './PokemonDetails';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path="/" render={ (props) => <Pokedex { ...props } pokemons={ pokemons } /> } />
+      <Route path="/" render={ (props) => <Pokedex { ...props } pokemons={ pokemons } /> } />
+      <Route path="/pokemon/:id" component={ PokemonDetails } />
     </BrowserRouter>
   );
 }
