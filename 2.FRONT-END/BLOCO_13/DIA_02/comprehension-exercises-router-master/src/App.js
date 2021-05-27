8870e94726c7ef8f45122b84c3f364689330d918
thosijulio <thosijulio@gmail.com>
2021-05-27 00:09:26 -0300

    resolucao exercicio 10

--- 2.FRONT-END/BLOCO_13/DIA_02/comprehension-exercises-router-master/src/App.js
+++ 2.FRONT-END/BLOCO_13/DIA_02/comprehension-exercises-router-master/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import About from './components/About';
 import Users from './components/Users';
 
@@ -8,9 +8,11 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={ Home } />
-        <Route path="/about" component={ About } />
-        <Route path="/users/:id" render={(props) => <Users { ...props } greetingMessage="Good Morning"/>} />
+        <Switch>
+          <Route path="/users/:id" render={(props) => <Users { ...props } greetingMessage="Good Morning"/>} />          
+          <Route path="/about" component={ About } />
+          <Route exact path="/" component={ Home } />
+        </Switch>
         <Link to="/"> Home </Link>
         <Link to="/about"> About </Link>
         <Link to="/users"> Users </Link>
