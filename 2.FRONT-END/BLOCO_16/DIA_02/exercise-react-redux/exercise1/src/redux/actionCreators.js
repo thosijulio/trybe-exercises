8dccf1446a34010989155cf04b52449c9512b577
thosijulio <thosijulio@gmail.com>
2021-06-29 14:12:47 -0300

    reiniciando exercicios do dia 16.2

--- 2.FRONT-END/BLOCO_16/DIA_02/exercise-react-redux/exercise1/src/redux/actionCreators.js
+++
@@ -1,6 +0,0 @@
-export const CHANGE_SIGNAL = 'CHANGE_SIGNAL';
-
-export const changeSignal = (payload) => ({
-  type: CHANGE_SIGNAL,
-  payload,
-});
--- 2.FRONT-END/BLOCO_16/DIA_02/exercise-react-redux/exercise2/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Cars from './Cars';
-import './App.css';
-
-export default function App() {
-  return (
-    <Cars/>
-  );
-}
