e696674f87783ba4f4e07a4b8e57b66142e021ac
thosijulio <thosijulio@gmail.com>
2021-06-30 10:01:59 -0300

    resolucao exercicio pos-aula 1

---
+++ 2.FRONT-END/BLOCO_16/DIA_02/POS-AULA/exercise1/src/TrafficSignal.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import redSignal from './images/redSignal.jpeg';
+import greenSignal from './images/greenSignal.jpeg';
+import yellowSignal from './images/yellowSignal.jpeg';
+import { changeSignal } from './redux/actionCreators';
+
+class TrafficSignal extends React.Component {
+
+  renderSignal = (signalColor) => {
+    if (signalColor === 'red') {
+      return redSignal;
+    }
+    if (signalColor === 'green') {
+      return greenSignal;
+    }
+    if (signalColor === 'yellow') {
+      return yellowSignal;
+    }
+    return null;
+  };
+
+  render() {
+
+    const { signalColor, changeSignals } = this.props;
+
+    return (
+      <div>
+        <div className="button-container">
+          <button type="button" onClick={() => changeSignals('red')}>Red</button>
+          <button type="button" onClick={() => changeSignals('yellow')}>Yellow</button>
+          <button type="button" onClick={() => changeSignals('green')}>Green</button>
+        </div>
+        <img className="signal" src={this.renderSignal(signalColor)} alt="" />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  signalColor: state.signal.color,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  changeSignals: (color) => dispatch(changeSignal(color)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TrafficSignal);
