54491e1a957a729647a047debd7f5f6784b7e62c
Julio H. Thosi <thosijulio@gmail.com>
2021-09-09 13:04:43 -0300

    Update exercicio1.sql

--- 3.BACK-END/BLOCO_20/DIA_01/POS_AULA/exercicio1.sql
+++ 3.BACK-END/BLOCO_20/DIA_01/POS_AULA/exercicio1.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `sakila`.`Filme` (
-  `FilmeId` INT NOT NULL DEFAULT +1,
+  `FilmeId` INT NOT NULL DEFAULT AUTO_INCREMENT,
   `Descricao` VARCHAR(100) NOT NULL,
   `AnoLancamento` INT NOT NULL,
   `Nota` INT NULL,
