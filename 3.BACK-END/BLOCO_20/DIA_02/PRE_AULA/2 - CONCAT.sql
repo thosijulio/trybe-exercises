91286600df2fabeabf42602ec3f9687954edb024
thosijulio <thosijulio@gmail.com>
2021-09-10 13:01:29 -0300

    resolucao exercicio 3 - CONCAT

--- 3.BACK-END/BLOCO_20/DIA_02/PRE_AULA/2 - CONCAT.sql
+++ 3.BACK-END/BLOCO_20/DIA_02/PRE_AULA/2 - CONCAT.sql
@@ -5,3 +5,6 @@ SELECT CONCAT (title, ' - ', release_year) AS 'Lançamento do Filme' FROM sakila
 
 -- Exercício 2:
 SELECT CONCAT (title, ' - ', rating) AS Classificação FROM sakila.film;
+
+-- Exercício 3:
+SELECT CONCAT (address, ', ', district) AS Endereço FROM sakila.address;
