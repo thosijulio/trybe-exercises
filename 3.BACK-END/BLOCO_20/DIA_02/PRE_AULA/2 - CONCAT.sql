c329ee698b71573693215e6b1e14fc76acf0d0f3
thosijulio <thosijulio@gmail.com>
2021-09-10 12:59:04 -0300

    resolucao exercicio 2 - CONCAT

--- 3.BACK-END/BLOCO_20/DIA_02/PRE_AULA/2 - CONCAT.sql
+++ 3.BACK-END/BLOCO_20/DIA_02/PRE_AULA/2 - CONCAT.sql
@@ -1,4 +1,7 @@
 USE sakila;
 
 -- Exercício 1:
-SELECT CONCAT (title, ' - ', release_year) as 'Lançamento do Filme' FROM sakila.film;
+SELECT CONCAT (title, ' - ', release_year) AS 'Lançamento do Filme' FROM sakila.film;
+
+-- Exercício 2:
+SELECT CONCAT (title, ' - ', rating) AS Classificação FROM sakila.film;
