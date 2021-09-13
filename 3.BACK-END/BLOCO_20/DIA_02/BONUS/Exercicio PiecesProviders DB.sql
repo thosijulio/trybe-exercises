85b42a20390809691ff742c4c4a5e2002214c1e2
thosijulio <thosijulio@gmail.com>
2021-09-13 11:25:28 -0300

    resolucao exercicio 2 - Bonus

--- 3.BACK-END/BLOCO_20/DIA_02/BONUS/Exercicio PiecesProviders DB.sql
+++ 3.BACK-END/BLOCO_20/DIA_02/BONUS/Exercicio PiecesProviders DB.sql
@@ -4,6 +4,8 @@ USE PiecesProviders;
 SELECT Piece, Price FROM PiecesProviders.Provides WHERE Provider = 'RBT';
 
 -- Exercício 2
+SELECT * FROM PiecesProviders.Provides ORDER BY Price DESC LIMIT 5;
+
 -- Exercício 3
 -- Exercício 4
 -- Exercício 5
