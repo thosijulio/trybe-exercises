ac29bcbe0e0f67fd6197cbf5ee32ab5601adddf3
thosijulio <thosijulio@gmail.com>
2021-09-13 10:51:14 -0300

    resolucao exercicio 4, 5 - Pos Aula 1

--- 3.BACK-END/BLOCO_20/DIA_02/POS-AULA/1 - Exercicios Scientists DB.sql
+++ 3.BACK-END/BLOCO_20/DIA_02/POS-AULA/1 - Exercicios Scientists DB.sql
@@ -1,1 +1,31 @@
+USE Scientists;
+
+-- Exercício 1
+SELECT 'This is SQL Exercise, Practice and Solution';
+
+-- Exercício 2
+SELECT 1, 2, 3;
+
+-- Exercício 3
 SELECT 10 + 15;
+
+-- Exercício 4
+SELECT 10 * 15 + 2;
+
+-- Exercício 5
+SELECT * FROM Scientists.Scientists;
+
+-- Exercício 6
+-- Exercício 7
+-- Exercício 8
+-- Exercício 9
+-- Exercício 10
+-- Exercício 11
+-- Exercício 12
+-- Exercício 13
+-- Exercício 14
+-- Exercício 15
+-- Exercício 16
+-- Exercício 17
+-- Exercício 18
+-- Exercício 19
