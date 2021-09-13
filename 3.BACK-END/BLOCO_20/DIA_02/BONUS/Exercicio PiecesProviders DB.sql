f59a79e62cc4a3f97b78d323f92964873ad70690
thosijulio <thosijulio@gmail.com>
2021-09-13 11:35:54 -0300

    resolucao exercicios 3, 4 e 5 - Bonus

--- 3.BACK-END/BLOCO_20/DIA_02/BONUS/Exercicio PiecesProviders DB.sql
+++ 3.BACK-END/BLOCO_20/DIA_02/BONUS/Exercicio PiecesProviders DB.sql
@@ -7,5 +7,10 @@ SELECT Piece, Price FROM PiecesProviders.Provides WHERE Provider = 'RBT';
 SELECT * FROM PiecesProviders.Provides ORDER BY Price DESC LIMIT 5;
 
 -- Exercício 3
+SELECT Provider, Price FROM PiecesProviders.Provides ORDER BY Price DESC LIMIT 4 OFFSET 2;
+
 -- Exercício 4
+SELECT * FROM PiecesProviders.Provides WHERE Provider = 'HAL' ORDER BY Price DESC;
+
 -- Exercício 5
+SELECT COUNT(Provider) FROM PiecesProviders.Provides WHERE Piece = 1;
