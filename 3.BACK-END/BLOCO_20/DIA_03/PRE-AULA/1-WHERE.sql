3f22bfd78f02e1bb77dde0fd76da1906ae850a20
thosijulio <thosijulio@gmail.com>
2021-09-14 12:18:44 -0300

    resolucao exercicio 4 e 5

--- 3.BACK-END/BLOCO_20/DIA_03/PRE-AULA/1-WHERE.sql
+++ 3.BACK-END/BLOCO_20/DIA_03/PRE-AULA/1-WHERE.sql
@@ -2,12 +2,12 @@ USE sakila;
 
 -- Exercício 1
 SELECT *
-FROM sakila.customer
+FROM customer
 WHERE email = 'LEONARD.SCHOFIELD@sakilacustomer.org';
 
 -- Exercício 2
 SELECT *
-FROM sakila.customer
+FROM customer
 WHERE active IS NOT TRUE
 AND store_id = 2
 AND first_name <> 'KENNETH'
@@ -15,14 +15,21 @@ ORDER BY first_name, last_name;
 
 -- Exercício 3
 SELECT title, description, release_year, replacement_cost
-FROM sakila.film
+FROM film
 WHERE rating <> 'NC-17'
 AND replacement_cost >= 18
 ORDER BY replacement_cost DESC, title
 LIMIT 100;
 
 -- Exercício 4
+SELECT COUNT(*) AS cliente_ativos
+FROM customer
+WHERE active IS TRUE
+AND store_id = 1;
 
 -- Exercício 5
-
+SELECT *
+FROM customer
+WHERE active IS NOT TRUE
+AND store_id = 1;
 -- Exercício 6
