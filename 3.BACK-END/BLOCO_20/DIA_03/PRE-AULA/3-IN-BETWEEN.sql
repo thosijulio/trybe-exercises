fbc4e2cde1f87f1505287b922d99d1d8145ebb38
thosijulio <thosijulio@gmail.com>
2021-09-15 09:12:24 -0300

    resolucao exercicio 1 - IN & BETWEEN

---
+++ 3.BACK-END/BLOCO_20/DIA_03/PRE-AULA/3-IN-BETWEEN.sql
@@ -0,0 +1,15 @@
+USE sakila;
+
+-- Exercício 1
+SELECT first_name, last_name, email
+FROM customer
+WHERE last_name IN('hicks', 'crawford', 'henry', 'boyd', 'mason', 'morales', 'kennedy')
+ORDER BY first_name;
+
+-- Exercício 2
+
+-- Exercício 3
+
+-- Exercício 4
+
+-- Exercício 5
