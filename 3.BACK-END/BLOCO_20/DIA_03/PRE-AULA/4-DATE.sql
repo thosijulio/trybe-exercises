6384bd01f9b9d49a4c0593ee199572307a9cd352
thosijulio <thosijulio@gmail.com>
2021-09-15 10:30:31 -0300

    resolucao exercicio 3 - DATE

--- 3.BACK-END/BLOCO_20/DIA_03/PRE-AULA/4-DATE.sql
+++ 3.BACK-END/BLOCO_20/DIA_03/PRE-AULA/4-DATE.sql
@@ -1,7 +1,13 @@
 USE sakila;
 
+-- Exercício 1
 SELECT * FROM payment WHERE DATE(payment_date) = '2005-05-25';
 
+-- Exercício 2
 SELECT * FROM payment WHERE DATE(payment_date) BETWEEN '2005-07-01' AND '2005-08-22';
 
-SELECT * FROM rental;
+-- Exercício 3
+SELECT DATE(rental_date), YEAR(rental_date), MONTH(rental_date), DAY(rental_date), HOUR(rental_date), MINUTE(rental_date), SECOND(rental_date)
+FROM rental WHERE rental_id = 10330;
+
+-- Exercício 4
