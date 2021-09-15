1aa0175abe5a26437dd3a76ab104094a8de73428
Julio H. Thosi <thosijulio@gmail.com>
2021-09-15 12:13:40 -0300

    Update 3-IN-BETWEEN.sql

--- 3.BACK-END/BLOCO_20/DIA_03/PRE-AULA/3-IN-BETWEEN.sql
+++ 3.BACK-END/BLOCO_20/DIA_03/PRE-AULA/3-IN-BETWEEN.sql
@@ -13,7 +13,7 @@ SELECT email FROM customer WHERE address_id BETWEEN 172 AND 176;
 SELECT * FROM payment WHERE payment_date BETWEEN '2005/05/01' AND '2005/08/01';
 
 -- Exercício 4
-SELECT title, release_year, rental_duration from film WHERE rental_duration BETWEEN 3 AND 6
+SELECT title, release_year, rental_duration FROM film WHERE rental_duration BETWEEN 3 AND 6
 ORDER BY rental_duration DESC, title;
 
 -- Exercício 5
