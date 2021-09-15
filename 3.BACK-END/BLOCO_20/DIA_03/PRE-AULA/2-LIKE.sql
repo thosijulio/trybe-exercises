6dd8a0269f4959d020199d34e319dbd27e85b5c0
thosijulio <thosijulio@gmail.com>
2021-09-15 08:56:10 -0300

    resolucao exercicio 4

--- 3.BACK-END/BLOCO_20/DIA_03/PRE-AULA/2-LIKE.sql
+++ 3.BACK-END/BLOCO_20/DIA_03/PRE-AULA/2-LIKE.sql
@@ -10,7 +10,7 @@ SELECT * FROM film WHERE description LIKE '%CHINA';
 SELECT * FROM film WHERE description LIKE '%GIRL%' AND title LIKE '%LORD';
 
 -- Exercício 4
-
+SELECT * FROM film WHERE title LIKE '___GON%';
 -- Exercício 5
 
 -- Exercício 6
