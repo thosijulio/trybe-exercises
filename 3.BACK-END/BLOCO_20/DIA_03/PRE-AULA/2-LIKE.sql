d2326928c2ee1d174bec0978b7e3a31c948fc394
thosijulio <thosijulio@gmail.com>
2021-09-15 08:54:36 -0300

    resolucao exercicio 1, 2 ,3 - LIKE'

---
+++ 3.BACK-END/BLOCO_20/DIA_03/PRE-AULA/2-LIKE.sql
@@ -0,0 +1,18 @@
+USE sakila;
+
+-- Exercício 1
+SELECT * FROM film WHERE title LIKE '%ACE%';
+
+-- Exercício 2
+SELECT * FROM film WHERE description LIKE '%CHINA';
+
+-- Exercício 3
+SELECT * FROM film WHERE description LIKE '%GIRL%' AND title LIKE '%LORD';
+
+-- Exercício 4
+
+-- Exercício 5
+
+-- Exercício 6
+
+-- Exercício 7
