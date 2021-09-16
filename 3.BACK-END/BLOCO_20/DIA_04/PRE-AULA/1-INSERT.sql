872bd7f4b3b8dc1785b09f269ba3bf6ffb848c5b
thosijulio <thosijulio@gmail.com>
2021-09-16 08:45:41 -0300

    resolucao exercicio 2 - INSERT

--- 3.BACK-END/BLOCO_20/DIA_04/PRE-AULA/1-INSERT.sql
+++ 3.BACK-END/BLOCO_20/DIA_04/PRE-AULA/1-INSERT.sql
@@ -7,7 +7,8 @@ INSERT INTO sakila.staff (staff_id, first_name, last_name, address_id, store_id,
 VALUES (22, 'Julio', 'Thosi', 1, 2, 'thosijulio');
 
 -- Exercício 2
-
+INSERT INTO sakila.staff (staff_id, first_name, last_name, address_id, store_id, username)
+VALUE (4, 'Ingrid', 'Castilho', 1, 2, 'castilhoingrid'), (5, 'Antonio', 'Thosi', 1, 2, 'thosiantonio');
 -- Exercício 3
 
 -- Exercício 4
