1a61a1e8ffea84184bf70dc8edfcacc949dc0c6d
thosijulio <thosijulio@gmail.com>
2021-09-16 09:09:29 -0300

    resolucao exercicio 3 - INSERT

--- 3.BACK-END/BLOCO_20/DIA_04/PRE-AULA/1-INSERT.sql
+++ 3.BACK-END/BLOCO_20/DIA_04/PRE-AULA/1-INSERT.sql
@@ -8,8 +8,11 @@ VALUES (22, 'Julio', 'Thosi', 1, 2, 'thosijulio');
 
 -- Exercício 2
 INSERT INTO sakila.staff (staff_id, first_name, last_name, address_id, store_id, username)
-VALUE (4, 'Ingrid', 'Castilho', 1, 2, 'castilhoingrid'), (5, 'Antonio', 'Thosi', 1, 2, 'thosiantonio');
+VALUES (4, 'Ingrid', 'Castilho', 1, 2, 'castilhoingrid'), (5, 'Antonio', 'Thosi', 1, 2, 'thosiantonio');
+
 -- Exercício 3
+INSERT INTO sakila.actor (first_name, last_name)
+SELECT first_name, last_name FROM sakila.customer LIMIT 5;
 
 -- Exercício 4
 
