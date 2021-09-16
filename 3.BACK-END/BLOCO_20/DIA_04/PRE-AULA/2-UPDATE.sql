5ce88edeb4fdd7de863f96867a5322eebed45c1e
thosijulio <thosijulio@gmail.com>
2021-09-16 10:30:01 -0300

    resolucao exercicio 3 - UPDATE

--- 3.BACK-END/BLOCO_20/DIA_04/PRE-AULA/2-UPDATE.sql
+++ 3.BACK-END/BLOCO_20/DIA_04/PRE-AULA/2-UPDATE.sql
@@ -8,5 +8,6 @@ UPDATE actor SET first_name = 'JULES' WHERE first_name = 'JULIA';
 UPDATE category SET name = 'Science Fiction' WHERE name = 'Sci-Fi';
 
 -- Exercício 3
+UPDATE film SET rental_rate = 25 WHERE `length` > 100 AND replacement_cost > 20 AND rating IN ('G', 'PG', 'PG-13');
 
 -- Exercício 4
