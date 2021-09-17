03d05fcfffb77d19961b79f70ce7f42d47758e3f
thosijulio <thosijulio@gmail.com>
2021-09-17 08:28:36 -0300

    resolucao exercicio 2 - Bonus

--- 3.BACK-END/BLOCO_20/DIA_04/BONUS/Bonus.sql
+++ 3.BACK-END/BLOCO_20/DIA_04/BONUS/Bonus.sql
@@ -1,6 +1,7 @@
 USE Pixar;
 
 -- Exercício 1
-UPDATE BoxOffice SET rating = 9 WHERE domestic_sales > 400000000;
+UPDATE BoxOffice SET rating = 9.0 WHERE domestic_sales > 400000000;
 
 -- Exercício 2
+UPDATE BoxOffice SET rating = 6.0 WHERE domestic_sales > 200000000 AND international_sales < 300000000;
