37789f4c9ea933303ca7b0d4485f58bbfad8a120
thosijulio <thosijulio@gmail.com>
2021-09-17 07:56:55 -0300

    resolucao exercicio 1, 2 e 4 - Pos-aula

---
+++ 3.BACK-END/BLOCO_20/DIA_04/POS-AULA/1-Exercises.sql
@@ -0,0 +1,27 @@
+USE Pixar;
+SET SQL_SAFE_UPDATES = 0;
+
+-- Exercício 1
+INSERT INTO Movies(title, director, `year`, length_minutes)
+VALUES
+	('Monstros SA', 'Pete Docter', 2001, 92),
+    ('Procurando Nemo', 'Jon Lasseter', 2003, 107),
+    ('Os Incríveis', 'Brad Bird', 2004, 116),
+    ('WALL-E', 'Pete Docter', 2008, 104);
+    
+-- Exercício 2
+SELECT id FROM Movies WHERE title = 'Procurando Nemo';
+INSERT INTO BoxOffice (movie_id, rating, domestic_sales, international_sales)
+VALUE (17, 6.8, 450000000, 370000000);
+
+
+-- Exercício 3
+UPDATE Movies SET director = 'Andrew Staton' WHERE title = 'Procurando Nemo';
+
+-- Exercício 4
+
+-- Exercício 5
+
+-- Exercício 6
+
+-- Exercício 7
