2cd333c41c09550e2773e13eaaeb6176d63905df
thosijulio <thosijulio@gmail.com>
2021-09-17 08:30:12 -0300

    resolucao exercicio 3 - Bonus

--- 3.BACK-END/BLOCO_20/DIA_04/BONUS/Bonus.sql
+++ 3.BACK-END/BLOCO_20/DIA_04/BONUS/Bonus.sql
@@ -5,3 +5,8 @@ UPDATE BoxOffice SET rating = 9.0 WHERE domestic_sales > 400000000;
 
 -- Exercício 2
 UPDATE BoxOffice SET rating = 6.0 WHERE domestic_sales > 200000000 AND international_sales < 300000000;
+
+-- Exercício 3
+SELECT id FROM Movies WHERE length_minutes < 100;
+DELETE FROM BoxOffice WHERE movie_id = 16;
+DELETE FROM Movies WHERE length_minutes < 100;
