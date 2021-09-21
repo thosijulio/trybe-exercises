6332e2436f348e4dbada6fef05acb2a44d5b984f
thosijulio <thosijulio@gmail.com>
2021-09-21 11:05:32 -0300

    resolucao exercicio 2 pre aula - if e case

--- 3.BACK-END/BLOCO_21/DIA_01/PRE-AULA/2-CONDICIONAIS.sql
+++ 3.BACK-END/BLOCO_21/DIA_01/PRE-AULA/2-CONDICIONAIS.sql
@@ -6,3 +6,15 @@ IF (title = 'ACE GOLDFINGER', 'Já assisti a esse filme', 'Não conheço o filme
 FROM sakila.film;
 
 -- Exercício 2
+SELECT 
+  title,
+  rating,
+  CASE
+    WHEN rating = 'G' THEN 'Livre para todos'
+    WHEN rating = 'PG' THEN 'Não recomendado para menores de 10 anos'
+    WHEN rating = 'PG-13' THEN 'Não recomendado para menores de 13 anos'
+    WHEN rating = 'R' THEN 'Não recomendado para menores de 17 anos'
+    ELSE 'Proibido para menores de idade'
+  END AS 'Público alvo'
+FROM sakila.film
+LIMIT 10;
