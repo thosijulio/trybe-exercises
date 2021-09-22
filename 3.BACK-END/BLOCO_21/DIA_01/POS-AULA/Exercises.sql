7e1406b6eaa21df33a63d942b755aab3947b35e3
thosijulio <thosijulio@gmail.com>
2021-09-22 07:49:06 -0300

    resolucao exercicio 1 - Pos Aula

---
+++ 3.BACK-END/BLOCO_21/DIA_01/POS-AULA/Exercises.sql
@@ -0,0 +1,36 @@
+-- Exercício 1
+SELECT MAX(SALARY) FROM hr.employees;
+
+-- Exercício 2
+
+-- Exercício 3
+
+-- Exercício 4
+
+-- Exercício 5
+
+-- Exercício 6
+
+-- Exercício 7
+
+-- Exercício 8
+
+-- Exercício 9
+
+-- Exercício 10
+
+-- Exercício 11
+
+-- Exercício 12
+
+-- Exercício 13
+
+-- Exercício 14
+
+-- Exercício 15
+
+-- Exercício 16
+
+-- Exercício 17
+
+-- Exercício 18
