e7c3ae1f9de90ea3a86c594ca0642382f15cd9b6
thosijulio <thosijulio@gmail.com>
2021-09-23 10:20:05 -0300

    resolucao exercicio 4 - INNER JOIN (PRE-AULA)

--- 3.BACK-END/BLOCO_21/DIA_02/PRE-AULA/1.INNER-JOIN.sql
+++ 3.BACK-END/BLOCO_21/DIA_02/PRE-AULA/1.INNER-JOIN.sql
@@ -23,4 +23,15 @@ FROM
     sakila.address AS a ON a.address_id = c.address_id
 ORDER BY c.first_name
 LIMIT 100;
+
 -- Exercício 4
+SELECT * FROM sakila.customer;
+SELECT * FROM sakila.address;
+SELECT 
+    C.first_name, C.email, C.address_id, A.address, A.district
+FROM
+    sakila.customer AS C
+        INNER JOIN
+    sakila.address AS A ON C.address_id = A.address_id
+WHERE
+    C.first_name LIKE '%rene%';
