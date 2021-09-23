f802f6d6fed753b3040d2ffc730ad1ec02f2258b
thosijulio <thosijulio@gmail.com>
2021-09-23 09:46:00 -0300

    resolucao exercicio 2 - INNER JOIN (PRE-AULA)

--- 3.BACK-END/BLOCO_21/DIA_02/PRE-AULA/1.INNER-JOIN.sql
+++ 3.BACK-END/BLOCO_21/DIA_02/PRE-AULA/1.INNER-JOIN.sql
@@ -1,4 +1,15 @@
 USE sakila;
 
+-- Exercício 1
 SELECT a.actor_id, a.first_name, f.film_id FROM sakila.actor AS a
-JOIN film_actor AS f ON f.actor_id = a.actor_id;
+INNER JOIN film_actor AS f ON f.actor_id = a.actor_id;
+
+-- Exercício 2
+SELECT c.first_name, c.last_name, c.address_id, a.address, a.district
+FROM sakila.customer AS c
+INNER JOIN sakila.address AS a
+ON a.address_id = c.address_id;
+
+-- Exercício 3
+
+-- Exercício 4
