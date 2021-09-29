33d6fa5673206b9a5b0e2c7435559cc9966f6f21
thosijulio <thosijulio@gmail.com>
2021-09-29 13:17:16 -0300

    resolucao exercicio 2 - PRE AULA (UNION E UNION ALL)

--- 3.BACK-END/BLOCO_21/DIA_02/PRE-AULA/3.UNION.sql
+++ 3.BACK-END/BLOCO_21/DIA_02/PRE-AULA/3.UNION.sql
@@ -7,3 +7,21 @@ UNION ALL SELECT
     first_name, last_name
 FROM
     sakila.staff;
+    
+-- Exercício 2
+SELECT 
+    first_name
+FROM
+    sakila.customer
+WHERE
+    first_name LIKE '%TRACY%' 
+UNION SELECT 
+    first_name
+FROM
+    sakila.actor
+WHERE
+    first_name LIKE '%JE%';
+
+-- Exercício 3
+
+-- Exercício 4
