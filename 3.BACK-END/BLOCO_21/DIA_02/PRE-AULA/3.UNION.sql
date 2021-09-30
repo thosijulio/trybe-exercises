7388a63fc23cb188aff05d9fc5f9fa20e08ea25d
thosijulio <thosijulio@gmail.com>
2021-09-30 06:43:38 -0300

    resolucao exercicio 3 - Pre Aula (UNION e UNION ALL)

--- 3.BACK-END/BLOCO_21/DIA_02/PRE-AULA/3.UNION.sql
+++ 3.BACK-END/BLOCO_21/DIA_02/PRE-AULA/3.UNION.sql
@@ -23,5 +23,7 @@ WHERE
     first_name LIKE '%JE%';
 
 -- Exercício 3
+(SELECT first_name FROM sakila.actor ORDER BY actor_id DESC LIMIT 5)
+UNION (SELECT first_name FROM sakila.staff LIMIT 1) UNION (SELECT first_name FROM sakila.customer LIMIT 5 OFFSET 15) ORDER BY first_name;
 
 -- Exercício 4
