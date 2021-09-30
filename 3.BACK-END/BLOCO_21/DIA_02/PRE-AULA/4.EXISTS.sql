0bd0a70b7481fb469d95d8fd5985db080a83e13a
thosijulio <thosijulio@gmail.com>
2021-09-30 07:15:27 -0300

    resolucao exercicios 2 e 3 - Pre Aula (EXISTS)

--- 3.BACK-END/BLOCO_21/DIA_02/PRE-AULA/4.EXISTS.sql
+++ 3.BACK-END/BLOCO_21/DIA_02/PRE-AULA/4.EXISTS.sql
@@ -14,3 +14,28 @@ WHERE
             B.Id = BL.book_id);
             
 -- EXERCÍCIO 2
+SELECT 
+    Id, title
+FROM
+    hotel.Books AS B
+WHERE
+    EXISTS( SELECT 
+            *
+        FROM
+            hotel.Books_Lent AS BL
+        WHERE
+            B.Id = BL.book_id)
+        AND title LIKE '%lost%';
+        
+-- Exercício 3
+SELECT 
+    `Name`
+FROM
+    hotel.Customers AS C
+WHERE
+    NOT EXISTS( SELECT 
+            *
+        FROM
+            CarSales AS CS
+        WHERE
+            CS.CustomerId = C.CustomerId)
