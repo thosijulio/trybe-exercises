2dd5eefdb00509cdc83d9e1dc0512f0cb7c749f6
thosijulio <thosijulio@gmail.com>
2021-09-30 16:42:03 -0300

    Resolucao exercicio 2 - Bonus

--- 3.BACK-END/BLOCO_21/DIA_02/BONUS/Bonus.sql
+++ 3.BACK-END/BLOCO_21/DIA_02/BONUS/Bonus.sql
@@ -9,3 +9,17 @@ FROM
     Pixar.BoxOffice AS BO ON BO.movie_id = M.id AND BO.rating > 8
 WHERE
     M.theater_id IS NOT NULL;
+
+-- Exercício 2
+SELECT 
+    M1.title,
+    M1.length_minutes,
+    M2.title,
+    M2.length_minutes,
+    M1.director
+FROM
+    Pixar.Movies AS M1,
+    Pixar.Movies AS M2
+WHERE
+    M1.director = M2.director
+        AND M1.title <> M2.title;
