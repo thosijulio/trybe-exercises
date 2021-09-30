26d7ab8bc9840624f7d6fc6fd728a430078e3b45
thosijulio <thosijulio@gmail.com>
2021-09-30 07:32:40 -0300

    resolucao exercicios 2 e 3 - Pós Aula

--- 3.BACK-END/BLOCO_21/DIA_02/POS-AULA/Exercicios.sql
+++ 3.BACK-END/BLOCO_21/DIA_02/POS-AULA/Exercicios.sql
@@ -8,3 +8,21 @@ FROM
         INNER JOIN
     Pixar.BoxOffice AS BO ON BO.movie_id = M.id;
     
+-- Exercício 2
+SELECT 
+    M.title, BO.international_sales, BO.domestic_sales
+FROM
+    Pixar.Movies AS M
+        INNER JOIN
+    Pixar.BoxOffice AS BO ON BO.movie_id = M.id
+WHERE
+    BO.international_sales > BO.domestic_sales;
+    
+-- Exercício 3
+SELECT 
+    M.title, BO.rating
+FROM
+    Pixar.Movies AS M
+        INNER JOIN
+    Pixar.BoxOffice AS BO ON BO.movie_id = M.id
+ORDER BY rating DESC;
