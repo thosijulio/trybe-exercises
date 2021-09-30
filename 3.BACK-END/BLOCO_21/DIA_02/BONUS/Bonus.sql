d2e141e4f2fd5ba5f7e80f8c42ad39fd3ada8911
thosijulio <thosijulio@gmail.com>
2021-09-30 16:49:00 -0300

    Resolucao exercicio 3 - Bonus

--- 3.BACK-END/BLOCO_21/DIA_02/BONUS/Bonus.sql
+++ 3.BACK-END/BLOCO_21/DIA_02/BONUS/Bonus.sql
@@ -23,3 +23,28 @@ FROM
 WHERE
     M1.director = M2.director
         AND M1.title <> M2.title;
+        
+-- Exercício 3 - PT1
+SELECT 
+    M.title
+FROM
+    Pixar.Movies AS M
+WHERE
+    M.id IN (SELECT 
+            BO.movie_id
+        FROM
+            Pixar.BoxOffice AS BO
+        WHERE
+            BO.domestic_sales + BO.international_sales >= 500000000)
+        AND M.length_minutes > 110;
+        
+-- Exercício 3 - PT2
+SELECT 
+    M.title
+FROM
+    Pixar.Movies AS M
+        INNER JOIN
+    Pixar.BoxOffice AS BO ON BO.movie_id = M.id
+WHERE
+    BO.domestic_sales + BO.international_sales >= 500000000
+        AND M.length_minutes > 110;
