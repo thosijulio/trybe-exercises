7116090818560a4180a0a656d3ac7e981a476a19
thosijulio <thosijulio@gmail.com>
2021-10-21 14:18:25 -0300

    resolucao exercicio 2

--- 3.BACK-END/BLOCO_25/DIA_01/exercicios/imc.js
+++ 3.BACK-END/BLOCO_25/DIA_01/exercicios/imc.js
@@ -1,3 +1,7 @@
-const calcImc = (peso, altura) => peso * (altura / 2);
+const calcImc = (peso, altura) => {
+  console.log(`Peso: ${peso} - Altura: ${altura}`);
+  const imc = peso * (altura / 2);
+  console.log(`IMC: ${imc}`);
+};
 
-module.exports({ calcImc });
+module.exports = { calcImc };
