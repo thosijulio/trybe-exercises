4124f7a9f4bc64e82f39a276d87383bcbeac3993
thosijulio <thosijulio@gmail.com>
2021-10-21 14:23:18 -0300

    resolucao exercicio 3

--- 3.BACK-END/BLOCO_25/DIA_01/exercicios/imc.js
+++ 3.BACK-END/BLOCO_25/DIA_01/exercicios/imc.js
@@ -1,7 +1,12 @@
-const calcImc = (peso, altura) => {
-  console.log(`Peso: ${peso} - Altura: ${altura}`);
+const readlineSync = require('readline-sync');
+
+const calcImc = () => {
+  const peso = readlineSync.questionFloat('Digite o seu peso: \n');
+  const altura = readlineSync.questionFloat('Digite a sua altura: \n');
   const imc = peso * (altura / 2);
   console.log(`IMC: ${imc}`);
 };
+
+calcImc();
 
 module.exports = { calcImc };
