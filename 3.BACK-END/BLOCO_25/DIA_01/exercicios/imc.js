2e787a07b36e31cae6f3764702868ca9c0510c7c
thosijulio <thosijulio@gmail.com>
2021-10-21 14:58:32 -0300

    resolucao exercicio 4

--- 3.BACK-END/BLOCO_25/DIA_01/exercicios/imc.js
+++ 3.BACK-END/BLOCO_25/DIA_01/exercicios/imc.js
@@ -3,8 +3,19 @@ const readlineSync = require('readline-sync');
 const calcImc = () => {
   const peso = readlineSync.questionFloat('Digite o seu peso: \n');
   const altura = readlineSync.questionFloat('Digite a sua altura: \n');
-  const imc = peso * (altura / 2);
-  console.log(`IMC: ${imc}`);
+  const imc = peso / (altura * 2);
+
+  const getImcSituation = (imc) => {
+    let response = '';
+      if (imc < 18.5 ) response = 'Abaixo do peso (magreza)'
+      else if (imc >= 18.5 && imc <= 24.9 ) response = 'Peso normal'
+      else if (imc >= 25 && imc <= 29.9 ) response = 'Acima do peso (sobrepeso)'
+      else if (imc >= 30 && imc <= 34.9 ) response = 'Obesidade grau I'
+      else if (imc >= 35 && imc <= 39.9 ) response = 'Obesidade grau II'
+      else response = 'Obesidade graus III e IV';
+    return response;
+  }
+  console.log(`IMC: ${imc}. ${getImcSituation(imc)}.`);
 };
 
 calcImc();
