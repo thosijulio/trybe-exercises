e9959eaeb3117181be342347ed45b98bcf4155da
thosijulio <thosijulio@gmail.com>
2021-10-21 15:12:31 -0300

    resolucao exercicio 6

--- 3.BACK-END/BLOCO_25/DIA_01/exercicios/imc.js
+++ 3.BACK-END/BLOCO_25/DIA_01/exercicios/imc.js
@@ -7,14 +7,15 @@ const calcImc = () => {
 
   const getImcSituation = (imc) => {
     let response = '';
-      if (imc < 18.5 ) response = 'Abaixo do peso (magreza)'
-      else if (imc >= 18.5 && imc <= 24.9 ) response = 'Peso normal'
-      else if (imc >= 25 && imc <= 29.9 ) response = 'Acima do peso (sobrepeso)'
-      else if (imc >= 30 && imc <= 34.9 ) response = 'Obesidade grau I'
-      else if (imc >= 35 && imc <= 39.9 ) response = 'Obesidade grau II'
+    if (imc < 18.5 ) response = 'Abaixo do peso (magreza)';
+    if (imc >= 18.5 && imc <= 24.9 ) response = 'Peso normal';
+    if (imc >= 25 && imc <= 29.9 ) response = 'Acima do peso (sobrepeso)';
+    if (imc >= 30 && imc <= 34.9 ) response = 'Obesidade grau I';
+    if (imc >= 35 && imc <= 39.9 ) response = 'Obesidade grau II'
       else response = 'Obesidade graus III e IV';
     return response;
-  }
+  };
+
   console.log(`IMC: ${imc}. ${getImcSituation(imc)}.`);
 };
 
