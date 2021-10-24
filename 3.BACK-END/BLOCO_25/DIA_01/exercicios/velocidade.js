3cb56ff36d99c119175f439db6711eea346125ff
thosijulio <thosijulio@gmail.com>
2021-10-24 08:07:47 -0300

    organizando pastas

--- 3.BACK-END/BLOCO_25/DIA_01/exercicios/velocidade.js
+++
@@ -1,9 +0,0 @@
-const readlineSync = require('readline-sync');
-
-const calcVelMedia = () => {
-  const distancia = readlineSync.questionInt('Digite a distância: ');
-  const velocidade = readlineSync.questionInt('Digite a velocidade: ');
-  console.log(`Velocidade média: ${distancia / velocidade}`);
-}
-
-module.exports = calcVelMedia;
