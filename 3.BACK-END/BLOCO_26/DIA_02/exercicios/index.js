0774f55eb1c6eb65c98c9510467d9c1ab4b9a61f
thosijulio <thosijulio@gmail.com>
2021-11-02 16:40:51 -0300

    resolucao exercicio 3

--- 3.BACK-END/BLOCO_26/DIA_02/exercicios/index.js
+++ 3.BACK-END/BLOCO_26/DIA_02/exercicios/index.js
@@ -1,9 +1,21 @@
 const exercicio1 = require('./exercicio1');
 
-const num1 = Math.floor(Math.random() * 100 + 1);
-const num2 = Math.floor(Math.random() * 100 + 1);
-const num3 = Math.floor(Math.random() * 100 + 1);
+const randomNumbers = Array.from({ length: 3 }).map(() => Math.floor(Math.random() * 100 + 1));
 
-exercicio1(num1, num2, num3)
-  .then((result) => console.log(result))
-  .catch((reject) => console.log(reject));
+const exercicio2 = () => {
+  exercicio1(...randomNumbers)
+    .then((result) => console.log(`exercicio2: ${result}`))
+    .catch((reject) => console.log(`exercicio2: ${reject}`));
+};
+
+const exercicio3 = async () => {
+  try {
+    const result = await exercicio1(...randomNumbers);
+    console.log(`exercicio3: ${result}`);
+  } catch (error) {
+    console.log(`exercicio3: ${error}`);
+  }
+};
+
+exercicio2();
+exercicio3();
