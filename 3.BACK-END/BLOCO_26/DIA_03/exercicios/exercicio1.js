69ca58c2043a5d84941d873cc7a014516d01e8b8
thosijulio <thosijulio@gmail.com>
2021-11-03 21:27:12 -0300

    inicio do exercicio 1

---
+++ 3.BACK-END/BLOCO_26/DIA_03/exercicios/exercicio1.js
@@ -0,0 +1,7 @@
+const exerc1 = (number) => {
+  if (number > 0) return 'positivo'
+  else if (number < 0) return 'negativo'
+  else return 'neutro';
+};
+
+module.exports = exerc1;
---
+++ 3.BACK-END/BLOCO_26/DIA_03/tests/exercicio1.js
@@ -0,0 +1,9 @@
+const { expect, assert } = require('chai');
+const exerc1 = require('../exercicios/exercicio1');
+
+describe('A funcao "exerc1" devera retornar: ', () => {
+  it('Positivo, caso o numero passado como parametro seja positivo.', () => {
+    const result = exerc1(10);
+    expect(result).to.be.equal('positivo');
+  });
+});
