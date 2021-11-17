ff08c86547887c8dbd98604478814e9b6a1984da
thosijulio <thosijulio@gmail.com>
2021-11-17 11:31:58 -0300

    resolucao exercicio 2

--- 3.BACK-END/BLOCO_26/DIA_05/router/userRouter.js
+++ 3.BACK-END/BLOCO_26/DIA_05/router/userRouter.js
@@ -14,6 +14,19 @@ router.post(
   }
 );
 
+router.post(
+  '/login',
+  emailAuth,
+  passwordAuth,
+  (_req, res) => {
+    const token = (Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2)).substr(0, 12);
+    return res.status(200).json({ token });
+  },
+  (err, req, res, next) => {
+    return res.status(400).json({ message: 'email or password is incorrect' });
+  }
+);
+
 router.use((err, _req, res, _next) => {
   return res.status(401).json(err.message);
 });
