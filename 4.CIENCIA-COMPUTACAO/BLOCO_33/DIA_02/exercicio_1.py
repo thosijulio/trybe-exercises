14ec3acccc2f41a24651ecb873fc6f3bd88461c4
thosijulio <thosijulio@gmail.com>
2022-02-22 13:04:25 -0300

    resolucao exercicio 1

--- 4.CIENCIA-COMPUTACAO/BLOCO_33/DIA_02/exercicio_1.py
+++ 4.CIENCIA-COMPUTACAO/BLOCO_33/DIA_02/exercicio_1.py
@@ -1,6 +1,8 @@
-name = input('Digite nome:')
-count = len(name)
+def show_name(name):
+    count = len(name)
+    
+    while count >= 0:
+        print(name[0:count])
+        count -= 1
 
-while(count > 0):
-    print(name[0:count])
-    count -= 1
+show_name('Bruno')
