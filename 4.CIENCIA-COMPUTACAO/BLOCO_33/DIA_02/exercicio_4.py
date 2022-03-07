986257574acda4de333f0f47ddf21213cb261ee0
thosijulio <thosijulio@gmail.com>
2022-03-07 08:47:39 -0300

    resolucao exercicio 4

--- 4.CIENCIA-COMPUTACAO/BLOCO_33/DIA_02/exercicio_4.py
+++ 4.CIENCIA-COMPUTACAO/BLOCO_33/DIA_02/exercicio_4.py
@@ -10,14 +10,13 @@ def get_books(path):
 
 def get_percentage_by_category(category):
     books = get_books("books.json")
-    categories_count = {}
+    categories_count = { }
     for book in books:
         for category in book["categories"]:
-            if category:
-                if category in categories_count:
-                    categories_count[category] = 2
-                else:
-                    categories_count.update({category: 1})
+            if category in categories_count:
+                categories_count[category] += 1
+            else:
+                categories_count.update({ category: 1 })
     return categories_count
 
-print(get_percentage_by_category())
+print(get_percentage_by_category("Web Development"))
