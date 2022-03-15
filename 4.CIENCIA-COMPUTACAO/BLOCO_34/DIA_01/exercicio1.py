5c3be1fa93fc19876f8044aa2d8fdc59bbe7edd5
thosijulio <thosijulio@gmail.com>
2022-03-15 10:24:18 -0300

    resolucao exercicio 1

---
+++ 4.CIENCIA-COMPUTACAO/BLOCO_34/DIA_01/exercicio1.py
@@ -0,0 +1,21 @@
+class Retangulo:
+    def __init__(self, altura, base):
+        self.altura = altura
+        self.base = base
+    
+    def calcular_area(self):
+        area = self.base * self.altura
+        print("Area do retângulo:", area)
+    
+    def calcular_perimetro(self):
+        perimetro = 2 * (self.base + self.altura)
+        print("Perimetro do retangulo:", perimetro)
+
+
+novo_retangulo = Retangulo(10, 15)
+
+print(novo_retangulo.altura)
+print(novo_retangulo.base)
+
+novo_retangulo.calcular_area()
+novo_retangulo.calcular_perimetro()
