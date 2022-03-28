0569f0e861c85db21b8e59440ae08648cf0a3d19
thosijulio <thosijulio@gmail.com>
2022-03-28 09:47:30 -0300

    resolucao exercicio 1 - pre aula

---
+++ 4.CIENCIA-COMPUTACAO/BLOCO_34/DIA_03/PRE_AULA/exercicio1.py
@@ -0,0 +1,17 @@
+list = [1, 2, 3, 4, 6, 5, 9, 8, 6, 7, 0]
+
+iterator = list.__iter__()
+
+print(iterator.__next__())
+print(iterator.__next__())
+print(iterator.__next__())
+print(iterator.__next__())
+print(iterator.__next__())
+print(iterator.__next__())
+print(iterator.__next__())
+print(iterator.__next__())
+print(iterator.__next__())
+print(iterator.__next__())
+print(iterator.__next__())
+print(iterator.__next__())
+print(iterator.__next__())
